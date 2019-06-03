202cb95bf92b829ca376cdafc638bc0cf2836ee6
Samuel <alphaelena@gmail.com>
2019-06-03 13:12:48 -0400

    finished part 2 of exercise 20 - Invoice header

--- building_pdf/main.go
+++ building_pdf/main.go
@@ -6,43 +6,55 @@ import (
 	"github.com/jung-kurt/gofpdf"
 )
 
+const (
+	bannerHt = 94.0
+	xIndent  = 40.0
+)
+
 func main() {
 	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+
 	w, h := pdf.GetPageSize()
+
 	fmt.Printf("width=%v ,height=%v", w, h)
 	pdf.AddPage()
-	// Basic Text
-	pdf.MoveTo(0, 0)
-	pdf.SetFont("Arial", "B", 38)
-	_, lineHeight := pdf.GetFontSize()
-	pdf.SetTextColor(255, 0, 0)
-	pdf.Text(0, lineHeight, "Hello, World")
-	pdf.MoveTo(0, lineHeight*2.0)
+	// Top maroon Banner
+	pdf.SetFillColor(103, 60, 79)
+	pdf.Polygon([]gofpdf.PointType{
+		{0, 0},
+		{w, 0},
+		{w, bannerHt},
+		{0, bannerHt * 0.9},
+	}, "F")
+	// Bottom maroon Banner
+	pdf.Polygon([]gofpdf.PointType{
+		{0, h},
+		{0, h - (bannerHt * 0.2)},
+		{w, h - (bannerHt * 0.1)},
+		{w, h},
+	}, "F")
 
-	pdf.SetFont("times", "", 18)
-	pdf.SetTextColor(100, 100, 100)
-	_, lineHeight = pdf.GetFontSize()
-	pdf.MultiCell(0, lineHeight*1.5, "here is a lot of some text\nalot of text text text\n", gofpdf.BorderNone, gofpdf.AlignRight, false)
+	//Banner -- INVOICE
+	pdf.SetFont("Arial", "B", 40)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt := pdf.GetFontSize()
+	pdf.Text(xIndent, bannerHt-(bannerHt/2.0)+lineHt/3.1, "INVOICE")
 
-	//Basic shapes
+	//Banner -- Phone ,Email,domain
+	pdf.SetFont("arial", "", 12)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-2.0*124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\nalphaelena@gmail.com\nsamueldavidd", gofpdf.BorderNone, gofpdf.AlignRight, false)
+	//pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\njon@calhoun.io\nGophercises.com", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
-	pdf.SetFillColor(0, 255, 0)
-	pdf.SetDrawColor(0, 0, 255)
-	pdf.Rect(10, 100, 100, 100, "FD")
-	pdf.SetFillColor(100, 200, 200)
-	pdf.Polygon(
-		[]gofpdf.PointType{
-			{110, 250},
-			{160, 300},
-			{110, 350},
-			{60, 300}}, "F")
-
-	pdf.ImageOptions("images/jump.png", 275, 275, 92, 0, false, gofpdf.ImageOptions{
-		ReadDpi: true,
-	}, 0, "")
+	//Banner -- Our Address & Co info
+	pdf.SetFont("arial", "", 12)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "123 Fake St\nSome Town\n12345", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
 	drawGrid(pdf)
-	err := pdf.OutputFileAndClose("test.pdf")
+	err := pdf.OutputFileAndClose("test2.pdf")
 	if err != nil {
 		panic(err)
 	}
@@ -53,12 +65,17 @@ func drawGrid(pdf *gofpdf.Fpdf) {
 	pdf.SetTextColor(80, 80, 80)
 	pdf.SetDrawColor(200, 200, 200)
 	for x := 0.0; x < w; x += (w / 20.0) {
-
+		pdf.SetTextColor(200, 200, 200)
 		pdf.Line(x, 0, x, h)
 		_, lineHt := pdf.GetFontSize()
 		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
 	}
 	for y := 0.0; y < h; y += (h / 20.0) {
+		if y < bannerHt*0.9 {
+			pdf.SetTextColor(200, 200, 200)
+		} else {
+			pdf.SetTextColor(80, 80, 80)
+		}
 
 		pdf.Line(0, y, w, y)
 
