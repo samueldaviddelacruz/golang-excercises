af0efc5fdbf993d4e9cf49d3fbcbefbc1106a38c
Samuel <alphaelena@gmail.com>
2019-06-04 11:08:38 -0400

    Finished part 3 of exercise 20, Invoice summary

--- building_pdf/main.go
+++ building_pdf/main.go
@@ -53,11 +53,55 @@ func main() {
 	pdf.MoveTo(w-xIndent-124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
 	pdf.MultiCell(124.0, lineHt*1.5, "123 Fake St\nSome Town\n12345", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
-	drawGrid(pdf)
-	err := pdf.OutputFileAndClose("test2.pdf")
+	// Summary - Billed To, Invoice #, Date of Issue
+	_, sy := summaryBlock(pdf, xIndent, bannerHt+lineHt*2.0, "Billed To", "Client Name", "123 Client Address", "City,State,Country", "Postal Code")
+	summaryBlock(pdf, xIndent*2.0+lineHt*12.5, bannerHt+lineHt*2.0, "Invoice Number", "0000000134")
+	summaryBlock(pdf, xIndent*2.0+lineHt*12.5, bannerHt+lineHt*6.25, "Date of Issue", "05/29/2018")
+
+	// Summary Invoice Total
+	x, y := w-xIndent-124.0, bannerHt+lineHt*2.25
+	pdf.MoveTo(x, y)
+	pdf.SetFont("times", "", 14)
+	_, lineHt = pdf.GetFontSize()
+	pdf.SetTextColor(180, 180, 180)
+	pdf.CellFormat(124.0, lineHt, "Invoice Total", gofpdf.BorderNone, gofpdf.LineBreakNone, gofpdf.AlignRight, false, 0, "")
+	x, y = x+2.0, y+lineHt*1.5
+	pdf.MoveTo(x, y)
+	pdf.SetFont("times", "", 48)
+	_, lineHt = pdf.GetFontSize()
+	alpha := 58
+	pdf.SetTextColor(72+alpha, 42+alpha, 55+alpha)
+	totalUSD := "$1234.50"
+	pdf.CellFormat(124.0, lineHt, totalUSD, gofpdf.BorderNone, gofpdf.LineBreakNone, gofpdf.AlignRight, false, 0, "")
+	x, y = x-2.0, y+lineHt*1.25
+
+	if sy > y {
+		y = sy
+	}
+	x, y = xIndent-20.0, y+30.0
+	pdf.Rect(x, y, w-(xIndent*2.0)+40.0, 3.0, "F")
+	//drawGrid(pdf)
+	err := pdf.OutputFileAndClose("test3.pdf")
 	if err != nil {
 		panic(err)
 	}
+}
+func summaryBlock(pdf *gofpdf.Fpdf, x, y float64, title string, data ...string) (float64, float64) {
+	pdf.SetFont("times", "", 14)
+	pdf.SetTextColor(180, 180, 180)
+	_, lineHt := pdf.GetFontSize()
+	y = y + lineHt
+	//x, y = xIndent, bannerHt+lineHt*2.5
+	pdf.Text(x, y, title)
+
+	y = y + lineHt*1.5
+	pdf.SetTextColor(50, 50, 50)
+	for _, str := range data {
+		y = y + lineHt*1.25
+		pdf.Text(x, y, str)
+
+	}
+	return x, y
 }
 func drawGrid(pdf *gofpdf.Fpdf) {
 	w, h := pdf.GetPageSize()
